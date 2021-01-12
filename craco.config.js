3994a5363db4158bd3f53c031f9ff6e202f0ade3
yuluo <seaveany@163.com>
2021-01-12 15:39:15 +0800

    webpack配置

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const CracoLessPlugin = require('craco-less');
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
 const path = require('path');
 const resolve = dir => path.resolve(__dirname, dir);
 
@@ -14,12 +15,25 @@ module.exports = {
           },
         },
       },
-    },
+    }
   ],
   webpack: {
+    configure: (webpackConfig, {env, paths}) => {
+      paths.appBuild = 'docs'
+      webpackConfig.output = {
+        ...webpackConfig.output,
+        path: path.resolve(__dirname, 'docs'),
+        publicPath: '/react-ts/'
+      }
+      return webpackConfig
+    },
     alias: {
       '@': resolve('src'),
       'component': resolve('src/component')
-    }
+    },
+    plugins: [
+      // 查看打包进度
+      new SimpleProgressWebpackPlugin()
+    ]
   }
 };
