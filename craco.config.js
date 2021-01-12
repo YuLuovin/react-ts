af776972a4baf3544abfbcc15c1bea46f89d0693
yuluo <seaveany@163.com>
2021-01-12 11:30:17 +0800

    save

--- craco.config.js
+++ craco.config.js
@@ -17,6 +17,22 @@ module.exports = {
     },
   ],
   webpack: {
+    configure: (webpackConfig, {
+      env, paths
+    }) => {
+      // paths.appPath='public'
+      paths.appBuild = 'dist'
+      webpackConfig.output = {
+        ...webpackConfig.output,
+          // ...{
+          //   filename: whenDev(() => 'static/js/bundle.js', 'static/js/[name].js'),
+          //   chunkFilename: 'static/js/[name].js'
+          // },
+          path: path.resolve(__dirname, 'docs'), // 修改输出文件目录
+          publicPath: '/react-ts'
+      }
+      return webpackConfig
+    },
     alias: {
       '@': resolve('src'),
       'component': resolve('src/component')
